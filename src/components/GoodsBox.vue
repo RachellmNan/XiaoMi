18a7c9c162151b1d62c3e86220b00b0804c2fa80
SummeJuice <826015755@qq.com>
2020-07-29 23:50:33 +0800

    banner

---
+++ src/components/GoodsBox.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="GoodsBox">
+        <div class="title-wraper">
+            <span class="title">家电</span>
+            <span class="iconfont">&#xe625;</span>
+            <span class="category-item">查看全部</span>
+            <span class="category-item">热门</span>
+            <span class="category-item">电视影音</span>
+        </div>
+        <div class="goods-wraper">
+            <div class="poster" v-if="0">
+                <img src="/imgs/poster/0.jpg" alt="">
+            </div>
+            <div class="poster" v-if="1">
+                <img src="/imgs/poster/1.jpg" alt="">
+                <img src="/imgs/poster/2.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"GoodsBox"
+}
+</script>
+
+<style lang="scss">
+    .GoodsBox{
+        .title-wraper{
+            .title{
+                display: inline-block;
+                height: 58px;
+                line-height: 58px;
+                font-size: 22px;
+            }
+            span{
+                cursor: pointer;
+            }
+            .iconfont{
+                float: right;
+                display: block;
+                line-height: 26px;
+                margin-left: 10px;
+            }
+            .category-item{
+                float: right;
+                display: block;
+                margin-left: 10px;
+                font-size: 16px;
+                &:hover{
+                    &::after{
+                        // content: '';
+                        // display: block;
+                        // height: 2px;
+                        // background-color: #ff6700;
+                        opacity: 1;
+                    }
+                }
+                &::after{
+                    content: '';
+                    display: block;
+                    height: 2px;
+                    opacity: 0;
+                    background-color: #ff6700;
+                }
+            }
+            &::after{
+                content: "";
+                display: block;
+                clear: both;
+            }
+        }
+        .goods-wraper{
+            .poster{
+                float: left;
+                img{
+                    display: block;
+                    width: 234px;
+                    margin-bottom: 14px;
+                }
+            }
+            &::after{
+                content: "";
+                clear: both;
+                display: block;
+            }
+        }
+
+    }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,65 +1,193 @@
 <template>
     <div class="Index">
-        <div class="container">
-            <div class="swiper-box">
-                <swiper :options="swiperOptions">
-                    <swiper-slide><img src="/imgs/banner/swiper1.png"></swiper-slide>
-                    <swiper-slide><img src="/imgs/banner/swiper2.png"></swiper-slide>
-                    <swiper-slide><img src="/imgs/banner/swiper3.png"></swiper-slide>
-                    <swiper-slide><img src="/imgs/banner/swiper4.png"></swiper-slide>
-                    <div class="swiper-pagination" slot="pagination"></div>
-                </swiper>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+        <div class="wraper">
+            <div class="container">
+                <div class="swiper-box">
+                    <swiper :options="swiperOptions">
+                        <swiper-slide><img src="/imgs/banner/swiper1.png"></swiper-slide>
+                        <swiper-slide><img src="/imgs/banner/swiper2.png"></swiper-slide>
+                        <swiper-slide><img src="/imgs/banner/swiper3.png"></swiper-slide>
+                        <swiper-slide><img src="/imgs/banner/swiper4.png"></swiper-slide>
+                        <div class="swiper-pagination" slot="pagination"></div>
+                    </swiper>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+                <div class="site-category">
+                    <ul class="site-category-list">
+                        <li class="category-item">
+                            <a class="iconfont"><span>手机 电话卡</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of PhoneList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a  class="iconfont"><span>电视 盒子</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of TvList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>笔记本 显示器</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of MacList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>家电 插线板</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of MachineList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>出行 穿戴</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of WearList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>智能 路由器</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of RouterList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>电源 配件</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of PartsList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>健康 儿童</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of HealthList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>耳机 音箱</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of HeadsetList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="category-item">
+                            <a class="iconfont"><span>生活 箱包</span></a>
+                            <div class="category-extend">
+                                <ul class="extend-wraper" v-for="(items,index) of LifeList" :key="index">
+                                    <li class="extend-item" v-for="item of items" :key=item.id>
+                                        <img :src="item.img" alt="">
+                                        <span>{{item.title}}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="banners-wraper">
+                    <div class="option-wraper">
+                        <a class="item" href="javascript:;">
+                            <img src="/imgs/miao.png" alt="小米秒杀">
+                            <span>小米秒杀</span>
+                        </a >
+                        <a class="item" href="javascript:;">
+                            <img src="/imgs/tuan.png" alt="企业团购" >
+                            <span>企业团购</span>
+                        </a>
+                        <a class="item" href="javascript:;">
+                            <img src="/imgs/f.png" alt="F码通道" href="javascript:;">
+                            <span>F码通道</span>
+                        </a>
+                        <a class="item" href="javascript:;">
+                            <img src="/imgs/mifen.png" alt="米粉卡" href="javascript:;">
+                            <span>米粉卡</span>
+                        </a>
+                        <a class="item"  href="javascript:;">
+                            <img src="/imgs/change.png" alt="以旧换新" href="javascript:;">
+                            <span>以旧换新</span>
+                        </a>
+                        <a class="item" href="javascript:;">
+                            <img src="/imgs/huafei.png" alt="话费充值" href="javascript:;">
+                            <span>话费充值</span>
+                        </a>
+                    </div>
+                    <div class="banner-list">
+                        <a href="javascript:;">
+                            <img src="/imgs/banner/5.jpg" alt="">
+                        </a>
+                        <a href="javascript:;">
+                            <img src="/imgs/banner/6.jpg" alt="">
+                        </a>
+                        <a href="javascript:;">
+                            <img src="/imgs/banner/7.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="site-category">
-                <ul class="site-category-list">
-                    <li class="category-item">
-                        <a class="iconfont">手机 电话卡</a>
-                        <div class="category-detail">
-
-                        </div>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">电视 盒子</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">笔记本 显示器</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">家电 插线板</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">出行 穿戴</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">智能 路由器</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">电源 配件</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">健康 儿童</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">耳机 音箱</a>
-                    </li>
-                    <li class="category-item">
-                        <a class="iconfont">生活 箱包</a>
-                    </li>
-                </ul>
+        </div>
+        <div class="bottom-wraper">
+            <div class="banner-box">
+                <a href="javascript:;">
+                    <img src="/imgs/banner/phone.jpg" alt="">
+                </a>
             </div>
+            <goods-box></goods-box>
         </div>
     </div>
 </template>
 
 <script>
+import GoodsBox from '../components/GoodsBox'
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 export default {
     name:'Index',
     components:{
         Swiper,
-        SwiperSlide
+        SwiperSlide,
+        GoodsBox
     },
     data(){
         return {
@@ -75,13 +203,33 @@ export default {
                 },
                 autoplay:true,
                 delay:3000
-            }
+            },
+            PhoneList:[],
+            TvList:[],
+            MacList:[],
+            MachineList:[],
+            WearList:[],
+            RouterList:[],
+            PartsList:[],
+            HealthList:[],
+            HeadsetList:[],
+            LifeList:[]
         }
     },
     mounted(){
         this.axios.defaults.baseURL = ''
         this.axios.get('/mock/data.json').then((res)=>{
-            console.log(res)
+            this.PhoneList   = res["phone-list"]
+            this.TvList      = res["tv-list"]
+            this.MacList     = res["mac-list"]
+            this.MachineList = res["machine-list"]
+            this.WearList    = res["wear-list"]
+            this.RouterList  = res["router-list"]
+            this.PartsList   = res["parts-list"]
+            this.HealthList  = res["health-list"]
+            this.HeadsetList = res["headset-list"]
+            this.LifeList    = res["life-list"]
+            console.log(this.PartsList)
         })
     }
 }
@@ -89,12 +237,18 @@ export default {
 
 <style lang="scss">
 .Index{
+    background-color: whitesmoke;
+    .wraper{
+        background-color: #fff;
+    }
     .container{
         width: 1226px;
         min-width: 1226px;
         margin: auto;
+        background-color: whitesmoke;
         .swiper-box{
             position: relative;
+            height: 460px;
             .swiper-pagination{
                 width: 400px;
                 margin-left:750px;
@@ -134,7 +288,7 @@ export default {
         }
         .site-category{
             width: 234px;
-            height: 420px;
+            height: 460px;
             position: absolute;
             z-index: 5;
             top: 140px;
@@ -146,25 +300,134 @@ export default {
                     padding-left: 30px;
                     height: 42px;
                     line-height: 42px;
-                    position: relative;
                     &:hover{
                         background-color: #ff6700;
                     }
                     a{
                         color: #fff;
+                        height: 42px;
+                        display: inline-block;
+                        span{
+                            display: inline-block;
+                            width: 124px;
+                        }
                     }
-                    .iconfont::before{
+                    .iconfont::after{
                         content: '\e600';
                         display: inline-block;
-                        position: absolute;
-                        right: 20px;
-                        top: 0px;
+                        text-align: right;
+                        margin-left: 20px;
                         width: 30px;
-                        height: 30px;
+                        height: 42px;
+                        line-height: 42px;
                         font-size: 20px;
                         color: #e0e0e0;
                     }
+                    &:hover{
+                        .category-extend{
+                            height: 460px;
+                        }
+                    }               
+                    .category-extend{
+                        position: absolute;
+                        display: flex;
+                        left: 234px;
+                        top: 0px;
+                        height: 0px;
+                        background: #fff;
+                        overflow: hidden;
+                        box-shadow:0 8px 16px rgba(0,0,0,.18);
+                        .extend-wraper{
+                            width: 248px;
+                            .extend-item{
+                                padding: 18px 20px;
+                                width: 225px;
+                                img{
+                                    display: inline-block;
+                                    width: 40px;
+                                    height: 40px;
+                                    margin-right: 12px;
+                                    vertical-align: middle;
+                                    margin-right: 12px;
+                                }
+                                span{
+                                    vertical-align: middle;
+                                    display: inline-block;
+                                    line-height: 40px;
+                                    width: 172px;
+                                    transition: all .3s;
+                                }
+                                &:hover{
+                                    span{
+                                        color: #ff6700;
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
+            }
+        }
+        .banners-wraper{
+            margin: 20px 0;
+            .option-wraper{
+                float:left;
+                width: 234px;
+                height: 170px;
+                background-color: #5f5750;
+                a{
+                    float: left;
+                    display: block;
+                    width: 76px;
+                    height: 64px;
+                    padding-top: 18px;
+                    text-align: center;
+                    img{
+                        display: block;
+                        width: 24px;
+                        margin: 0 23px 4px;
+                    }
+                    span{
+                        color: #fff;
+                        display: block;
+                        
+                    }
+                }
+                &::after{
+                    content: '';
+                    display: block;
+                    clear: both;
+                }
+            }
+            .banner-list{
+                float: left;
+                margin-left: 14px;
+                a{
+                    margin-left: 15px;
+                }
+                a:first-of-type{
+                    margin-left: 0px;
+                }
+                img{
+                    width: 316px;
+                }
+            }
+            &::after{
+                    content: '';
+                    display: block;
+                    clear: both;
+            }
+
+        }
+    }
+    .bottom-wraper{
+        width: 1226px;
+        min-width: 1226px;
+        margin: auto;
+        .banner-box{
+            margin: 22px 0;
+            img{
+                width: 1226px;
             }
         }
     }
